17a962ccebab9baaa020f983e85ca3e5c542e9ca
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2024-01-03 15:41:10 +0900

    ロジックの簡略化、ページにブロック作成する機能を追加

--- index.js
+++ index.js
@@ -1,86 +1,87 @@
 async function createNewBlock({ uuid }, blockContent = "") {
   const block = await logseq.Editor.getBlock(uuid);
-  // 子が存在し、かつ、子が開かれている場合はカレントブロックの最初に要素を挿入し終了
-  if (block.children.length > 0) {
-    const closed = false; //How...
-    if (!closed) {
-      await createFirstChildToCurrent({ uuid }, blockContent);
-      return
-    }
+  // カレントブロックに子が存在し、かつ折りたたまれていない場合
+  if (!block["collapsed?"] && block.children.length > 0) {
+    // カレントブロックに最初の子を挿入
+    await logseq.Editor.insertBlock(uuid, blockContent, { sibling: false, before: true });
+  } else {
+    // カレントブロックの弟を挿入
+    await logseq.Editor.insertBlock(uuid, blockContent, { sibling: true, before: false });
   }
+}
 
-  // 次の要素が存在すれば、その前に要素を挿入
-  const nextSibling = await logseq.Editor.getNextSiblingBlock(uuid);
-  if (nextSibling) {
-    await logseq.Editor.insertBlock(nextSibling.uuid, blockContent, { before: true });
-    return
-  }
-
-  // 親ブロックが存在すれば、親ブロックの最後に要素を挿入
+async function _createFirstOrLastSibling({ uuid }, blockContent = "", addToLast = true) {
+  const index = addToLast ? -1 : 0;
+  const block = await logseq.Editor.getBlock(uuid);
   const parent = await logseq.Editor.getBlock(block.parent.id);
-  if (parent) {
-    await logseq.Editor.insertBlock(parent.uuid, blockContent);
-    return
-  }
-
-  // 親ブロックが存在しなければ、ページの最後に要素を挿入
-  const page = await logseq.Editor.getPage(block.page.id);
-  await logseq.Editor.appendBlockInPage(page.uuid, blockContent);
+  // parent.uuidに挿入すると、parentがpageだった場合に失敗するので、parent.children[0]の兄弟要素に挿入する
+  const firstUuid = Object.fromEntries([parent.children.at(index)]).uuid;
+  await logseq.Editor.insertBlock(firstUuid, blockContent, { sibling: true, before: true });
 }
 
 async function createFirstSibling({ uuid }, blockContent = "") {
-  const block = await logseq.Editor.getBlock(uuid);
-  const parent = await logseq.Editor.getBlock(block.parent.id);
-  const firstUuid = Object.fromEntries([parent.children[0]]).uuid;
-  logseq.Editor.insertBlock(firstUuid, blockContent, { before: true });
+  await _createFirstOrLastSibling({ uuid }, blockContent, true);
 }
 
 async function createLastSibling({ uuid }, blockContent = "") {
-  const block = await logseq.Editor.getBlock(uuid);
-  const parent = await logseq.Editor.getBlock(block.parent.id);
-  logseq.Editor.insertBlock(parent.uuid, blockContent);
+  await _createFirstOrLastSibling({ uuid }, blockContent, false);
 }
 
 async function createNextSibling({ uuid }, blockContent = "") {
-  const block = await logseq.Editor.getBlock(uuid);
-  const parent = await logseq.Editor.getBlock(block.parent.id);
-  const siblingUuidList = parent.children.map((v) => Object.fromEntries([v]).uuid);
-  const targetIndex = siblingUuidList.indexOf(block.uuid) + 1;
-  if (targetIndex === siblingUuidList.length) {
-    await logseq.Editor.insertBlock(parent.uuid, blockContent);
+  await logseq.Editor.insertBlock(uuid, blockContent, { sibling: true, before: false });
+}
+
+async function createPreviousSibling({ uuid }, blockContent = "") {
+  await logseq.Editor.insertBlock(uuid, blockContent, { sibling: true, before: true });
+}
+
+async function createFirstChildInCurrentBlock({ uuid }, blockContent = "") {
+  await logseq.Editor.insertBlock(uuid, blockContent, { sibling: false, before: true });
+}
+
+async function createLastChildInCurrentBlock({ uuid }, blockContent = "") {
+  await logseq.Editor.insertBlock(uuid, blockContent, { sibling: false, before: false });
+}
+
+async function _createFirstOrLastChildInPage(blockContent = "", addToLast = true) {
+  const pageOrBlock = await logseq.Editor.getCurrentPage();
+  if (pageOrBlock.parent) {
+    const block = pageOrBlock
+    _createFirstOrLastSibling(block, blockContent, addToLast);
+    return
+  }
+
+  const page = pageOrBlock;
+  if (addToLast) {
+    await logseq.Editor.appendBlockInPage(page.uuid, blockContent);
   } else {
-    await logseq.Editor.insertBlock(siblingUuidList[targetIndex], blockContent, { before: true });
+    await logseq.Editor.prependBlockInPage(page.uuid, blockContent);
   }
 }
 
-async function createPrevSibling({ uuid }, blockContent = "") {
-  await logseq.Editor.insertBlock({ uuid }, blockContent, { before: true });
+async function createFirstChildInCurrentPage({ _uuid }, blockContent = "") {
+  _createFirstOrLastChildInPage(blockContent, true);
 }
 
-async function createFirstChildToCurrent({ uuid }, blockContent = "") {
-  const block = await logseq.Editor.getBlock(uuid);
-  if (block.children.length === 0) {
-    insertLastChild(uuid);
-  } else {
-    const firstChildUuid = Object.fromEntries([block.children[0]]).uuid;
-    logseq.Editor.insertBlock(firstChildUuid, blockContent, { before: true });
-  }
+async function createLastChildInCurrentPage({ _uuid }, blockContent = "") {
+  _createFirstOrLastChildInPage(blockContent, false);
 }
 
-async function createLastChildToCurrent({ uuid }, blockContent = "") {
-  logseq.Editor.insertBlock(uuid, blockContent);
-}
 
 function main() {
-  logseq.App.registerCommandPalette({ key: 'createNewBlock', label: 'Create New Block', keybinding: { mode: "editing", binding: "alt+enter" }, }, createNewBlock);
+  logseq.App.registerCommandPalette({ key: 'createNewBlock', label: 'Create New Block', keybinding: { mode: "editing", binding: "alt+enter" } }, createNewBlock);
 
-  logseq.App.registerCommandPalette({ key: 'createFirstSibling', label: 'Create First Sibling', keybinding: { mode: "editing" }, }, createFirstSibling);
-  logseq.App.registerCommandPalette({ key: 'createLastSibling', label: 'Create Last Sibling', keybinding: { mode: "editing" }, }, createLastSibling);
-  logseq.App.registerCommandPalette({ key: 'createNextSibling', label: 'Create Next Sibling', keybinding: { mode: "editing" }, }, createNextSibling);
-  logseq.App.registerCommandPalette({ key: 'createPrevSibling', label: 'Create Prev Sibling', keybinding: { mode: "editing" }, }, createPrevSibling);
+  logseq.App.registerCommandPalette({ key: 'createFirstSibling', label: 'Create First Sibling', keybinding: { mode: "editing" } }, createFirstSibling);
+  logseq.App.registerCommandPalette({ key: 'createLastSibling', label: 'Create Last Sibling', keybinding: { mode: "editing" } }, createLastSibling);
+  logseq.App.registerCommandPalette({ key: 'createNextSibling', label: 'Create Next Sibling', keybinding: { mode: "editing" } }, createNextSibling);
+  logseq.App.registerCommandPalette({ key: 'createPreviousSibling', label: 'Create Previous Sibling', keybinding: { mode: "editing" }, }, createPreviousSibling);
 
-  logseq.App.registerCommandPalette({ key: 'createFirstChildToCurrent', label: 'Create First Child To Current', keybinding: { mode: "editing" }, }, createFirstChildToCurrent);
-  logseq.App.registerCommandPalette({ key: 'createLastChildToCurrent', label: 'Create Last Child To Current', keybinding: { mode: "editing", binding: "ctrl+alt+enter" }, }, createLastChildToCurrent);
+  logseq.App.registerCommandPalette({ key: 'createFirstChildInCurrentBlock', label: 'Create First Child In Current Block', keybinding: { mode: "editing" } }, createFirstChildInCurrentBlock);
+  logseq.App.registerCommandPalette({ key: 'createLastChildInCurrentBlock', label: 'Create Last Child In Current Block', keybinding: { mode: "editing", binding: "ctrl+alt+enter" } }, createLastChildInCurrentBlock);
+
+  logseq.App.registerCommandPalette({ key: 'createFirstChildInCurrentPage', label: 'Create First Child In Current Page', keybinding: { mode: "editing" } }, createFirstChildInCurrentPage);
+  logseq.App.registerCommandPalette({ key: 'createLastChildInCurrentPage', label: 'Create Last Child In CurrentPage', keybinding: { mode: "editing" } }, createLastChildInCurrentPage);
+
 };
 
 logseq.ready(main).catch(console.error)
